2d8f4b67b8694825cdfc198cc64a5fd71dd47c95
crocsman <49213404+crocsman@users.noreply.github.com>
2021-06-23 09:41:55 -0700

    added day highlight

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,10 @@
 
-var currentDay = new Date();
+currentDay = new Date();
+var date = (currentDay.getDate()-1);
 
 //loop thru ul https://www.sitepoint.com/community/t/looping-through-li-elements-in-ul/6049/2
-var day = document.getElementByClass("days");
-var days = day.getElementsByTagName("li");
+var pDays = document.getElementById("days");
+var days = pDays.getElementsByTagName("li");
     //use js time library to determine day to highlight current day
-    for (var i = 0; i < days.length; i++)
-    {
-        if(currentDay.getDate() == currentDay)
-        {
-            days[i].style.backgroundColor="yellow";
-        }
-    }   
+days[date].style.backgroundColor="yellow";
+    
