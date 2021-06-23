b00b20429807c95daafeac3b8bf9d0068241dba8
crocsman <49213404+crocsman@users.noreply.github.com>
2021-06-23 13:58:03 -0700

    moved to github

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,20 @@
 
-currentDay = new Date();
-var date = (currentDay.getDate()-1);
+//date highlight management
+currentTime = new Date();
+var date = (currentTime.getDate()-1);
 
-//loop thru ul https://www.sitepoint.com/community/t/looping-through-li-elements-in-ul/6049/2
-var pDays = document.getElementById("days");
-var days = pDays.getElementsByTagName("li");
-    //use js time library to determine day to highlight current day
+var idDay = document.getElementById("days");
+var days = idDay.getElementsByTagName("li");
+
+//use js time library to determine day to highlight current day
 days[date].style.backgroundColor="yellow";
-    
+ 
+//month name management
+let monthArr = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+let monthNum = currentTime.getMonth();
+
+
+var idMonth = document.getElementById("month");
+// html node ? https://stackoverflow.com/questions/43221594/change-a-href-innerhtml-without-id
+var month = idMonth.getElementsByTagName("p")[0].innerHTML = monthArr[monthNum];
+   
